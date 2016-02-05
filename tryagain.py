a7a239fc25a07de7a854609acbe0cebd46cfc50b
Thomas Feldmann <info@tfeldmann.de>
2016-02-05 11:55:11 +0100

    move tryagain into project root

--- tryagain.py
+++ tryagain.py
@@ -9,21 +9,23 @@
 """ tryagain: A simple and pythonic retry helper
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    Full documentation is at ___.
+    Full documentation is at http://tryagain.readthedocs.org
 
-    :copyright: (c) 2015 by Thomas Feldmann.
-    :license:   MIT, see LICENSE for more details.
+    :license: MIT, see LICENSE for more details.
 """
-
 import time
 import logging
+import inspect
 from functools import wraps
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('tryagain')
 
 
 __version__ = '1.0'
 
 
+# this code is taken from the itertools.repeat documentation. The function
+# from the itertools module cannot be used in this context because of a weird
+# behaviour in python: https://bugs.python.org/issue25926
 def _repeat(obj, times=None):
     # repeat(10, 3) --> 10 10 10
     if times is None:
@@ -34,16 +36,15 @@ def _repeat(obj, times=None):
             yield obj
 
 
-def _check_callable(func, allow_none=True):
+def _assert_callable(func, allow_none=True):
     if not (func is None and allow_none):
         if not callable(func):
             raise TypeError('{} is not callable'.format(func))
 
 
-def retry_call(func, max_attempts=None, exceptions=Exception, wait=0.0,
-               cleanup_hook=None, pre_retry_hook=None):
-    """
-        :param func (callable):
+def call(func, max_attempts=None, exceptions=Exception, wait=0.0,
+         cleanup_hook=None, pre_retry_hook=None):
+    """ :param func (callable):
             The function to retry. No arguments are passed to this function.
             If your function requires arguments, consider defining a separate
             function or use functools.partial / a lambda function.
@@ -56,7 +57,7 @@ def retry_call(func, max_attempts=None, exceptions=Exception, wait=0.0,
             An iterable of exceptions that should result in a retry.
 
         :param wait:
-            Specify how many seconds
+            Specify the waittime before the next attempt
 
         :param cleanup_hook:
             Can be set to a callable and will be called after an exception is
@@ -76,7 +77,7 @@ def retry_call(func, max_attempts=None, exceptions=Exception, wait=0.0,
             Exceptions that are raised when calling this hook are not caught.
 
         :returns:
-            - The result of calling the given `func`.
+            The result of calling the given `func`.
 
         :raises:
             Any exception which is
@@ -86,36 +87,50 @@ def retry_call(func, max_attempts=None, exceptions=Exception, wait=0.0,
     """
     # we check the callables in advance to prevent raising exceptions
     # after making the first attempt
-    _check_callable(func, allow_none=False)
-    _check_callable(cleanup_hook, allow_none=True)
-    _check_callable(pre_retry_hook, allow_none=True)
+    _assert_callable(func, allow_none=False)
+    _assert_callable(cleanup_hook, allow_none=True)
+    _assert_callable(pre_retry_hook, allow_none=True)
     if not (max_attempts is None or max_attempts >= 1):
         raise ValueError('max_attempts must be None or an integer >= 1')
+
+    # if the user sets the waittime to a fixed value (int or float) we create
+    # a function which always returns this fixed value. This way we avoid
+    # having to make this decision in the retry loop.
+    wait_func = wait if type(wait) not in [int, float] else lambda _: wait
+
+    # check if our wait_func is a function with a single argument
+    if len(inspect.signature(wait_func).parameters) != 1:
+        raise ValueError(
+            'The wait function must be a function with a single argument')
+
+    def log_failed_attempt(attempt, error):
+        if max_attempts is None:
+            nr_display = '{}'.format(attempt)
+        else:
+            nr_display = '{0} / {1}'.format(attempt, max_attempts)
+        logger.debug('Attempt {nr} at calling {func} failed ({msg})'
+                     .format(nr=nr_display, func=func, msg=error))
 
     for attempt, f in enumerate(_repeat(func, max_attempts), start=1):
         try:
             return f()
         except exceptions as e:
-            if max_attempts is not None:
-                nr_display = '#{0} / {1}'.format(attempt, max_attempts)
-            else:
-                nr_display = '#{}'.format(attempt)
-            logger.debug('Attempt {nr} at calling {func} failed ({msg})'
-                         .format(nr=nr_display, func=f, msg=e))
+            log_failed_attempt(attempt=attempt, error=e)
             if cleanup_hook is not None:
                 cleanup_hook()
             if attempt == max_attempts:
                 raise
             if wait:
-                time.sleep(wait)
+                waittime = wait_func(attempt)
+                time.sleep(waittime)
             if pre_retry_hook is not None:
                 pre_retry_hook()
 
 
-def retry(*args, **kwargs):
-    """ A decorator factory for retry_call().
+def retries(*args, **kwargs):
+    """ A decorator factory for tryagain.call().
 
-        Wrap your function in @retriable(...) to give it retry powers!
+        Wrap your function in @retries(...) to give it retry powers!
 
         Arguments:
             Same as for `retry`, with the exception
